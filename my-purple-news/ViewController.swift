6649a44abaf1e0f4c6292902e1d72e2ab392af2d
Kevin Bluer <kevin@bluer.com>
2015-04-25 20:33:41 +0800

    adding some runtime styling on the buttons

--- my-purple-news/ViewController.swift
+++ my-purple-news/ViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var btnInternational: UILabel!
+    @IBOutlet weak var buttonSports: UIButton!
+    @IBOutlet weak var buttonBusiness: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        buttonSports.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        buttonSports.layer.cornerRadius = 10
+        buttonSports.layer.borderWidth = 2
+        buttonSports.layer.borderColor = UIColor( red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0 ).CGColor
+        
+        buttonBusiness.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        buttonBusiness.layer.cornerRadius = 10
+        buttonBusiness.layer.borderWidth = 2
+        buttonBusiness.layer.borderColor = UIColor( red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0 ).CGColor
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
